62d78b3f97653b7c383d7e544d68f89148703d6d
NandaRaja96 <nandaraja96@gmail.com>
2021-12-25 19:54:42 +0530

    GitHub

---
+++ src/test/java/com/add/Testing.java
@@ -0,0 +1,14 @@
+package com.add;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Testing {
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.youtube.com/watch?v=SJ5gFysRsAM");
+}
+}
